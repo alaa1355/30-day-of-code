32734ac8147d936323b258bc685a0683956aeefb
Alaa Albadarein <aoalbadareen16@sci.edu.just.jo>
2021-03-17 21:48:40 +0200

    test solve

---
+++ day2.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+if __name__ == "__main__":
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    tip_amount = meal_cost * tip_percent / 100
+    tax_amount = meal_cost * tax_percent / 100
+    total_cost = round(meal_cost + tip_amount + tax_amount)
+    print( str(total_cost))
